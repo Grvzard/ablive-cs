70e3f17b7b3c0b7e9e36e57fbe417a9cb9e1c52e
Grvzard <grvzard@outlook.com>
2023-07-27 20:13:43 +0800

    deps(server): support for pydantic_v2, add "pydantic-settings"

--- ablive-server/app/core/config.py
+++ ablive-server/app/core/config.py
@@ -1,15 +1,13 @@
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', case_sensitive=False)
+
     MONGO_URI: str
     API_KEY: str
     API_PREFIX: str = ""
-
-    class Config:
-        env_file = '.env'
-        case_sensitive = False
 
 
 settings = Settings()  # type: ignore
